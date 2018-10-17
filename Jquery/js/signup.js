3a80e40e10658aec4ccf79f94b78f8df2e663a28
mairako <mkotsovoulou@acg.edu>
2018-10-17 14:46:48 +0300

    foundations demo

--- Jquery/js/signup.js
+++ Jquery/js/signup.js
@@ -7,7 +7,7 @@ $("#newsletter").change(function() {
        $("#email").hide();
 });
 
-/* If you want a "live" validation uncomment this code */
+/* If you want a Jquery Custom "live" validation uncomment the code below */
 /*
 $("#pass1").keyup(function() {
   if($(this).val().length < 5) {
@@ -28,13 +28,12 @@ $("#pass2").keyup(function() {
 
 
 /* If you want to use the validator library from JQuery uncomment this code */
-
+// Remember that the validator uses the name HTML attribute and not the id
  $("#registration").validate({
             rules: {
                	username: {
-					          required: true,
-					          minlength: 2
-				          },
+					              required: true,
+					              minlength: 2 },
                 pass1: { minlength: 5,
                          required : true },
                 pass2: {
@@ -43,7 +42,7 @@ $("#pass2").keyup(function() {
             },
             messages: {
                username: {
-					            required: "Please enter a username",
+					            required : "Please enter a username",
 					            minlength: "Your username must consist of at least 2 characters"
 				              },
                 pass1: { required : "Enter Password",
@@ -55,9 +54,8 @@ $("#pass2").keyup(function() {
 }); //End Document.READY
 
 /* If you want to validate the form when the button is clicked uncomment this code */
-/*
+
 function validateForm() {
-     $("#registration").click(function(){
         $(".error").hide();
         var isValid = true;
         var pass1 = $("#pass1").val();
@@ -65,17 +63,15 @@ function validateForm() {
          if (pass1.length < 5) {
              $("#pass1").after('<span class="error">Password should be more than 5.</span>');
            isValid=false;
-  }
+          }
 
         if (pass1 != pass2) {
            $("#pass2").after('<span class="error">Passwords do not match.</span>');
           isValid=false;
         }
-
      return isValid;
-     });
 }
-*/
+
 
 $.validator.setDefaults({
 		submitHandler: function() {
