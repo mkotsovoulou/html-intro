a4bff12f0afc20a5ee525ea460ee56c765662a80
mairako <mkotsovoulou@acg.edu>
2018-11-12 14:24:26 +0200

    templates

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+include('start.php');
+include('navbar.php'); ?>
+<main role="main">
+<h1>
+  Maira Demo Site
+</h1>
+<?php
+include('carousel.php');
+?>
+<h2>
+  More titles
+</h2>
+<?php
+include('main.php');
+include('calltoaction.php');?>
+</main>
+<?php
+include('footer.php');
+include('end.php');
+?>
--- login.php
+++ login.php
@@ -1,31 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8" />
-	<title>Login</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link href="container.css" rel="stylesheet">
-</head>
-<body>
-
-    <?php
-	include("dbFunctions.php");
-	?>
-
-	<center><h1>Login</h1></center>
-	<div class="container container-small">
-
-  	<form action="loginCheck.php" method="post">
-  		<div class="form-group">
-    		<label for="id">ID</label>
-    		<input type="text" class="form-control" name ="id" id="id" placeholder="ex. 854595">
-  		</div>
-    		<label for="email">Email</label>
-    		<input type="text" name ="email" class="form-control" id="email" placeholder="ex. gate7@gate7.gr">
-        <br>
-		<button type="submit" class="btn btn-primary">Save</button>
-	</form>
+<?php
+include('start.php');
+include('navbar.php');
+include('dbFunctions.php');
+?>
+<center><h1>Login</h1></center>
+<div class="container container-small">
+  <form action="loginCheck.php" method="post">
+    <div class="form-group">
+      <label for="id">ID</label>
+      <input type="text" class="form-control" name ="id" id="id" placeholder="ex. 854595">
     </div>
+    <label for="email">Email</label>
+    <input type="text" name ="email" class="form-control" id="email" placeholder="ex. gate7@gate7.gr">
+    <br>
+    <button type="submit" class="btn btn-primary">Save</button>
+  </form>
+</div>
+<?php
+include('footer.php');
+include('end.php');
+?>
 
-</body>
-</html>
+
